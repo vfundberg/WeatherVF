47a5c564bc5f6981832b3653e0edbc205739e56c
Fundberg <vfundberg@gmail.com>
2018-03-26 12:43:50 +0200

    Startpage done with picture, added buttons to the about-page and the weather-page.

---
+++ WeatherVF/StartViewController.swift
@@ -0,0 +1,33 @@
+//
+//  StartViewController.swift
+//  WeatherVF
+//
+//  Created by Victor Fundberg on 2018-03-26.
+//  Copyright © 2018 Victor Fundberg. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
